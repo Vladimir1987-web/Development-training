3eb660da4cc5ffdaf91fd0a66e5659541f04f920
Владимир Федотов <vladimir.fedotov1987@yandex.ru>
2025-05-26 21:23:47 +0300

    verified defget_date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+import datetime
+
 from masks import get_mask_account, get_mask_card_number
 
 
@@ -23,19 +25,16 @@ def mask_account_card(card_or_account_number: str) -> str:
         return str_mask_number
 
 
-def get_date(date: str) -> str:
+def get_date(info_date: str) -> str:
     """
     Принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")
-    :param date:
+    :param info_date:
     :return:
     """
-    str_date = ''
-    pass
-
-    return  str_date
+    return f'{info_date[8:10]}.{info_date[5:7]}.{info_date[0:4]}'
 
 
 if __name__ in '__main__':
     print(mask_account_card('Счет 64686473678894779589'))
-    #print(get_date('2024-03-11T02:26:18.671407'))
+    print(get_date('2024-03-11T02:26:18.671407'))
