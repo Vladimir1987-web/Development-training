c0c39c853b5c7757ab827938a670666cb01ebc5d
Владимир Федотов <vladimir.fedotov1987@yandex.ru>
2025-05-26 19:09:25 +0300

    commit_2: skeleton of the function

---
+++ src/widget.py
@@ -0,0 +1,22 @@
+def mask_account_card(card_or_account_number: str) -> str:
+    """
+    Обрабатывает информацию как о картах, так и о счетах
+    :param card_or_account_number:
+    :return:
+    """
+    str_mask_number = ''
+    pass
+
+    return str_mask_number
+
+
+def get_date(date: str) -> str:
+    str_date = ''
+    pass
+
+    return  str_date
+
+
+if __name__ in '__maine__':
+    print(mask_account_card('Maestro 1596837868705199'))
+    print(get_date('2024-03-11T02:26:18.671407'))
