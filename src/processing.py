9946a721c1e6173be5f15ecf9563ad2d54e946f4
Владимир Федотов <vladimir.fedotov1987@yandex.ru>
2025-06-04 18:17:30 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -8,6 +8,11 @@ def filter_by_state(operation_list: list[dict[str, Any]], state: str = "EXECUTED
     return list((transaction for transaction in operation_list if transaction.get("state") == state))
 
 
+def sort_by_date(operation_list: list[dict[str, Any]]) -> list[dict[str, Any]]:
+    """Функция сортировки даты"""
+    return sorted(operation_list, key=lambda item: item['date'], reverse=True)
+
+
 if __name__ in "__main__":
     print(
         filter_by_state(
@@ -16,6 +21,17 @@ if __name__ in "__main__":
                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
+
+    print(
+        sort_by_date(
+            [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
             ]
         )
     )
