d90abb3ddd8a85b3cec406c6441e007506184af7
Владимир Федотов <vladimir.fedotov1987@yandex.ru>
2025-06-05 17:59:03 +0300

    add correction widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_or_account_number: Union[str]) -> str:
