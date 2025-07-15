4899ab8adea664521f0cdb1cc8826c9427dd5f96
Владимир Федотов <vladimir.fedotov1987@yandex.ru>
2025-07-15 21:32:26 +0300

    add logers for utils.py and mask.py

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,34 @@
+import logging
 from typing import Union
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename=r"C:\Training\Python-development\Project\pythonProjectBank\logs\masks.log",
+    encoding="utf-8",  # Запись логов в файл
+    filemode="w",
+)
+app_logger = logging.getLogger("masks.py")
 
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
     """
     Принимает на вход номер карты и возвращает ее маску
     """
+    app_logger.info("Принятие на вход номера карты.")
     if not isinstance(card_number, (int | str)):
+        app_logger.error("Ошибка типа данных.")
         raise TypeError("Ошибка типа данных")
     # Переводим числа в строку и убираем пробелы в номере карты
     str_card_number = str(card_number)
     number_card = str_card_number.replace(" ", "")
 
     if len(number_card) != 16:
+        app_logger.error("Неверная длина номера карты!")
         raise ValueError("Неверная длина номера карты!")
 
     if not number_card.isdigit():
+        app_logger.error("Недопустимые символы в номере карты!")
         raise ValueError("Недопустимые символы в номере карты!")
 
     # Вставляем пробелы после каждой четвёртой цифры
@@ -28,6 +42,7 @@ def get_mask_card_number(card_number: Union[str, int]) -> str:
             mask_cart_list[i] = "*"
     mask_card_number = "".join(mask_cart_list)
 
+    app_logger.info("Вывод маски номера карты.")
     return mask_card_number
 
 
@@ -37,8 +52,9 @@ def get_mask_account(number_cart: Union[str, int]) -> str:
     :param number_cart:
     :return:
     """
-
+    app_logger.info("Принятие на вход номера счета.")
     if not isinstance(number_cart, (int | str)):
+        app_logger.error("Ошибка типа данных.")
         raise TypeError("Ошибка типа данных")
 
     # Переводим числа в строку и убираем пробелы в номере счёта
@@ -46,13 +62,16 @@ def get_mask_account(number_cart: Union[str, int]) -> str:
     number_cart = cart_str.replace(" ", "")
 
     if not number_cart.isdigit():
+        app_logger.error("Недопустимые символы в номере карты!")
         raise ValueError("Недопустимые символы в номере карты!")
 
     if len(number_cart) != 20:
+        app_logger.error("Неверная длина номера карты!")
         raise ValueError("Неверная длина номера карты!")
 
     # Делаем маску номера счёта
     number_mask = str(number_cart[-4:])
+    app_logger.info("Вывод маски номера счёта.")
     return f"**{number_mask}"
 
 
